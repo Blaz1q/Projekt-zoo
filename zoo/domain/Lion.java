f7125ef5182370ea89102da0210deb36586307b1
Oliwia <oliwiakucharska1@o2.pl>
2025-12-09 11:28:43 +0100

    Add Lion class extending Mammal and Trainable

---
+++ zoo/domain/Lion.java
@@ -0,0 +1,13 @@
+package zoo.domain;
+
+public class Lion extends Mammal implements Trainable{
+
+    public Lion(String name, int age, String furcolor) {
+        super(name, age, furcolor);
+    }
+
+    @Override
+    public String performTrick() {
+        return "Skok na bunge";
+    }
+}
